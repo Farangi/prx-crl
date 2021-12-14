c5ddf9a0ae052153b29b9fb8950f434dedf50631
Saad Zafar <saadzafar@Saads-MacBook-Air.local>
2021-12-15 01:25:29 +0500

    Task done

--- javascripts/pages/index.js
+++ javascripts/pages/index.js
@@ -2,4 +2,9 @@
 // Avoid using jquery if possible.
 $(document).ready(() => {
   console.log('Index page');
+  const form = document.querySelector("#main-form");
+  const btn = document.querySelector(".button")
+  form.addEventListener('submit', () => {
+    btn.classList.toggle("button--loading")
+  })
 });
